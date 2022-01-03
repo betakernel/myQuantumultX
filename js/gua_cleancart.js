b1146e39efb70b6712c0dd691438b31a214d9fe3
betakernel <sxbg@vip.qq.com>
2022-01-03 12:55:31 +0800

    Update gua_cleancart.js

--- js/gua_cleancart.js
+++ js/gua_cleancart.js
@@ -37,7 +37,7 @@ pin3@&@不清空👉该pin不清空
 */
 let jdSignUrl = "https://jd.smiek.tk/jdcleancatr_21102717" // 算法url
 let cleancartRun = 'true'
-let cleancartProducts = '*@&@机油,火花塞,滤清器,刹车,电瓶,牙刷,沐浴,儿童,ASICS,Mizuno,数据,眼镜,书,充电,MFi'
+let cleancartProducts = '*@&@机油,火花塞,滤清器,刹车,电瓶,牙刷,沐浴,ASICS,Mizuno,数据,眼镜,书,充电,MFi'
 
 const $ = new Env('清空购物车');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
